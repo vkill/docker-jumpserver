f0d79a2b43e372e7044106c60ebcf6dec4651778
vkill <vkill.net@gmail.com>
2018-12-09 17:26:18 +0800

    update

--- compose/docker_build.sh
+++ compose/docker_build.sh
@@ -6,6 +6,11 @@ script_path=$(cd $(dirname $0) ; pwd -P)
 script_path_root="${script_path}/"
 
 docker build \
-  -t vkill/jumpserver:1.4.4-alpine \
-  -f "${script_path_root}../jumpserver/Dockerfile" \
-  "${script_path_root}../jumpserver"
+  -t vkill/jumpserver-core:1.4.4-alpine \
+  -f "${script_path_root}../core/Dockerfile" \
+  "${script_path_root}../core"
+
+docker build \
+  -t vkill/jumpserver-coco:1.4.4-alpine \
+  -f "${script_path_root}../coco/Dockerfile" \
+  "${script_path_root}../coco"
