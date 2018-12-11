fc31994d53053bb38a54e2b77ba32eee5c43cf14
vkill <vkill.net@gmail.com>
2018-12-11 09:14:54 +0800

    update

---
+++ docker_build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+script_path=$(cd $(dirname $0) ; pwd -P)
+script_path_root="${script_path}/"
+
+docker build \
+  -t vkill/jumpserver-core:1.4.4-alpine \
+  -f "${script_path_root}core/Dockerfile" \
+  "${script_path_root}core"
+
+docker build \
+  -t vkill/jumpserver-coco:1.4.4-alpine \
+  -f "${script_path_root}coco/Dockerfile" \
+  "${script_path_root}coco"
+
+docker build \
+  -t vkill/jumpserver-luna:1.4.4-alpine \
+  -f "${script_path_root}luna/Dockerfile" \
+  "${script_path_root}luna"
+
+docker build \
+  -t vkill/jumpserver-frontend:1.4.4-alpine \
+  -f "${script_path_root}frontend/Dockerfile" \
+  "${script_path_root}frontend"
