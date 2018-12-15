bfaf73a9e47f8327ca311b08b55225222b07125e
vkill <vkill.net@gmail.com>
2018-12-15 13:41:50 +0800

    update

--- scripts/docker_build_on_local.sh
+++ scripts/docker_build_on_local.sh
@@ -6,21 +6,21 @@ script_path=$(cd $(dirname $0) ; pwd -P)
 script_path_root="${script_path}/"
 
 docker build \
-  -t vkill/jumpserver-core:1.4.4-alpine \
+  -t vkill/jumpserver-core:1.4.5-alpine \
   -f "${script_path_root}../core/Dockerfile" \
   "${script_path_root}../core"
 
 docker build \
-  -t vkill/jumpserver-coco:1.4.4-alpine \
+  -t vkill/jumpserver-coco:1.4.5-alpine \
   -f "${script_path_root}../coco/Dockerfile" \
   "${script_path_root}../coco"
 
 docker build \
-  -t vkill/jumpserver-luna:1.4.4-alpine \
+  -t vkill/jumpserver-luna:1.4.5-alpine \
   -f "${script_path_root}../luna/Dockerfile" \
   "${script_path_root}../luna"
 
 docker build \
-  -t vkill/jumpserver-frontend:1.4.4-alpine \
+  -t vkill/jumpserver-frontend:1.4.5-alpine \
   -f "${script_path_root}../frontend/Dockerfile" \
   "${script_path_root}../frontend"
